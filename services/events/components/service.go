00bc0d805f6d8a013938096e8d822e6cf431b390
harture <harture@localhost.localdomain>
2017-10-12 17:00:48 +0200

    EventBus scenario

--- services/events/components/service.go
+++ services/events/components/service.go
@@ -5,20 +5,61 @@ import (
 	"github.com/cloudtrust/keycloak-bridge/services/events/modules/console"
 	"github.com/cloudtrust/keycloak-bridge/services/events/transport/flatbuffers/events"
 	"github.com/cloudtrust/keycloak-bridge/services/events/transport"
+	"github.com/asaskevich/EventBus"
 )
 
 /*
 This is the interface that user services implement.
  */
-type Service interface {
+type MuxService interface {
 	Event(ctx context.Context, eventType string, obj []byte) (interface{}, error)
 }
 
+
 /*
  */
-func NewBasicService(consoleModule console.Service) Service {
+func NewBasicMuxService(bus EventBus.Bus) MuxService {
+	return &basicMuxService{
+		bus:bus,
+	}
+}
+
+type basicMuxService struct {
+	bus EventBus.Bus
+}
+
+func (u *basicMuxService)Event(ctx context.Context, eventType string, obj []byte) (interface{}, error) {
+	switch eventType {
+	case "AdminEvent":
+		var adminEvent *events.AdminEvent
+		adminEvent= events.GetRootAsAdminEvent(obj, 0)
+		u.bus.Publish("main:AdminEvent", ctx, adminEvent)
+		return "ok", nil
+	case "Event":
+		var event *events.Event
+		event= events.GetRootAsEvent(obj, 0)
+		u.bus.Publish("main:Event", ctx, event)
+		return "ok", nil
+	default:
+		var err transport.ErrInvalidArgument
+		err.InvalidParam = "Type"
+		return "bad", err
+	}
+}
+
+
+type Service interface {
+	Event(ctx context.Context, event events.Event) (interface{}, error)
+	AdminEvent(ctx context.Context, adminEvent events.AdminEvent) (interface{}, error)
+}
+
+
+
+/*
+ */
+func NewBasicService(console console.Service) Service {
 	return &basicService{
-		module:consoleModule,
+		module: console,
 	}
 }
 
@@ -26,21 +67,12 @@ type basicService struct {
 	module console.Service
 }
 
-func (u *basicService)Event(ctx context.Context, eventType string, obj []byte) (interface{}, error) {
-	switch eventType {
-	case "AdminEvent":
-		var adminEvent *events.AdminEvent
-		adminEvent= events.GetRootAsAdminEvent(obj, 0)
-		u.module.Print(ctx, string(adminEvent.Uid()), string(adminEvent.Time()))
-		return "ok", nil
-	case "Event":
-		var event *events.Event
-		event= events.GetRootAsEvent(obj, 0)
-		u.module.Print(ctx, string(event.Uid()), string(event.Time()))
-		return "ok", nil
-	default:
-		var err transport.ErrInvalidArgument
-		err.InvalidParam = "Type"
-		return "bad", err
-	}
+func (u *basicService)Event(ctx context.Context, event events.Event) (interface{}, error) {
+	u.module.Print(ctx, "Event", string(event.Uid()), string(event.Time()))
+	return nil, nil
+}
+
+func (u *basicService)AdminEvent(ctx context.Context, adminEvent events.AdminEvent) (interface{}, error) {
+	u.module.Print(ctx, "AdminEvent", string(adminEvent.Uid()), string(adminEvent.Time()))
+	return nil, nil
 }
