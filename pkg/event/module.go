440a4a97f2bd944ad33c30835ac4c5d241a63164
harture <harture414@gmail.com>
2019-02-21 21:09:34 +0100

    Fix data race

--- pkg/event/module.go
+++ pkg/event/module.go
@@ -40,12 +40,18 @@ func NewConsoleModule(logger log.Logger, esc ESClient, esIndex, componentName, c
 }
 
 func (cm *consoleModule) Print(_ context.Context, m map[string]string) error {
+	// Need to do a copy of the map to avoid data race
+	var mapCopy = make(map[string]string)
+	for k, v := range m {
+		mapCopy[k]=v
+	}
+
 	// Add component infos in the map
-	m["componentID"] = cm.componentID
-	m["componentName"] = cm.componentName
+	mapCopy["componentID"] = cm.componentID
+	mapCopy["componentName"] = cm.componentName
 
 	// Index data
-	err := cm.esClient.IndexData(cm.esIndex, "audit", m["uid"], m)
+	err := cm.esClient.IndexData(cm.esIndex, "audit", mapCopy["uid"], mapCopy)
 	if err != nil {
 		return err
 	}
