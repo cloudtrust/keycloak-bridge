a2f2ddbfbe3aa4292c0ebd702508bff7d9175aa3
Francis PEROT <francis.perot@elca.ch>
2019-05-16 14:28:16 +0200

    fixup! fixup! Make mocks being automatically generated

--- pkg/event/mock_test.go
+++ pkg/event/mock_test.go
@@ -4,3 +4,4 @@ package event
 //go:generate mockgen -destination=./mock/tracking.go -package=mock -mock_names=Sentry=Sentry github.com/cloudtrust/keycloak-bridge/pkg/event Sentry
 //go:generate mockgen -destination=./mock/tracing.go -package=mock -mock_names=Tracer=Tracer,Span=Span,SpanContext=SpanContext github.com/opentracing/opentracing-go Tracer,Span,SpanContext
 //go:generate mockgen -destination=./mock/instrumenting.go -package=mock -mock_names=Histogram=Histogram github.com/go-kit/kit/metrics Histogram
+//go:generate mockgen -destination=./mock/logging.go -package=mock -mock_names=Logger=Logger github.com/go-kit/kit/log Logger
