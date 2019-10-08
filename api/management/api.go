9bc426566e52bdbf3b0b4f48af85f9d4c5d7da3f
rpo <harture414@gmail.com>
2019-10-08 13:59:22 +0200

    New flags for realm config

--- api/management/api.go
+++ api/management/api.go
@@ -92,8 +92,12 @@ type RealmCustomConfiguration struct {
 	DefaultRedirectURI              *string `json:"default_redirect_uri,omitempty"`
 	APISelfAuthenticatorMgmtEnabled *bool   `json:"api_self_authenticator_mgmt_enabled"`
 	APISelfPasswordChangeEnabled    *bool   `json:"api_self_password_change_enabled"`
+	APISelfMailEditionEnabled       *bool   `json:"api_self_mail_edition_enabled"`
+	APISelfDeleteAccountEnabled     *bool   `json:"api_self_delete_account_enabled"`
 	UISelfAuthenticatorMgmtEnabled  *bool   `json:"ui_self_authenticator_mgmt_enabled"`
 	UISelfPasswordChangeEnabled     *bool   `json:"ui_self_password_change_enabled"`
+	UISelfMailEditionEnabled        *bool   `json:"ui_self_mail_edition_enabled"`
+	UISelfDeleteAccountEnabled      *bool   `json:"ui_self_delete_account_enabled"`
 }
 
 // RequiredAction type
