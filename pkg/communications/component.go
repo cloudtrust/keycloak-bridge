d1ba45c132474adbded48b87c7af66b14941c0b3
Lorenceau Pablo <pablo.lorenceau@elca.ch>
2020-12-04 18:54:50 +0100

    [SCPR-32] add component

---
+++ pkg/communications/component.go
@@ -0,0 +1,61 @@
+package communications
+
+import (
+	"context"
+
+	cs "github.com/cloudtrust/common-service"
+	api "github.com/cloudtrust/keycloak-bridge/api/communications"
+	internal "github.com/cloudtrust/keycloak-bridge/internal/keycloakb"
+	kc "github.com/cloudtrust/keycloak-client"
+)
+
+// KeycloakCommunicationsClient interface exposes methods we need to call to send requests to Keycloak communications API
+type KeycloakCommunicationsClient interface {
+	SendEmail(accessToken string, reqRealmName string, realmName string, emailRep kc.EmailRepresentation) error
+	SendSMS(accessToken string, realmName string, smsRep kc.SMSRepresentation) error
+}
+
+// Component interface exposes methods used by the bridge API
+type Component interface {
+	SendEmail(ctx context.Context, reqRealmName string, realmName string, emailRep api.EmailRepresentation) error
+	SendSMS(ctx context.Context, realmName string, smsRep api.SMSRepresentation) error
+}
+
+type component struct {
+	keycloakCommunicationsClient KeycloakCommunicationsClient
+	logger                       internal.Logger
+}
+
+// NewComponent returns the communications component.
+func NewComponent(keycloakCommunicationsClient KeycloakCommunicationsClient, logger internal.Logger) Component {
+	return &component{
+		keycloakCommunicationsClient: keycloakCommunicationsClient,
+		logger:                       logger,
+	}
+}
+
+func (c *component) SendEmail(ctx context.Context, reqRealmName string, realmName string, emailRep api.EmailRepresentation) error {
+	var accessToken = ctx.Value(cs.CtContextAccessToken).(string)
+
+	var kcEmailRep = kc.EmailRepresentation{}
+	emailRep.ExportToKeycloak(&kcEmailRep)
+	err := c.keycloakCommunicationsClient.SendEmail(accessToken, reqRealmName, realmName, kcEmailRep)
+	if err != nil {
+		c.logger.Warn(ctx, "err", err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *component) SendSMS(ctx context.Context, realmName string, smsRep api.SMSRepresentation) error {
+	var accessToken = ctx.Value(cs.CtContextAccessToken).(string)
+
+	var kcSmsRep = kc.SMSRepresentation{}
+	smsRep.ExportToKeycloak(&kcSmsRep)
+	err := c.keycloakCommunicationsClient.SendSMS(accessToken, realmName, kcSmsRep)
+	if err != nil {
+		c.logger.Warn(ctx, "err", err.Error())
+		return err
+	}
+	return nil
+}
