873ba512a57820beb34c07cfa694abf6e3cd67e6
Lorenceau Pablo <pablo.lorenceau@elca.ch>
2020-12-07 15:23:00 +0100

    [SCPR-32] finish component test

--- pkg/communications/mock_test.go
+++ pkg/communications/mock_test.go
@@ -1,3 +1,4 @@
 package communications
 
 //go:generate mockgen -destination=./mock/keycloak_communications_client.go -package=mock -mock_names=KeycloakCommunicationsClient=KeycloakCommunicationsClient github.com/cloudtrust/keycloak-bridge/pkg/communications KeycloakCommunicationsClient
+//go:generate mockgen -destination=./mock/logging.go -package=mock -mock_names=Logger=Logger github.com/cloudtrust/common-service/log Logger
