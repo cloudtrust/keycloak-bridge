9cc6660432d4750209079e905b0fcbb5a8d6f227
Lorenceau Pablo <pablo.lorenceau@elca.ch>
2020-12-07 10:39:38 +0100

    [SCPR-32] add http

---
+++ pkg/communications/http.go
@@ -0,0 +1,38 @@
+package communications
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/cloudtrust/keycloak-bridge/internal/constants"
+
+	commonhttp "github.com/cloudtrust/common-service/http"
+	"github.com/cloudtrust/common-service/log"
+	"github.com/go-kit/kit/endpoint"
+	http_transport "github.com/go-kit/kit/transport/http"
+)
+
+const (
+	reqBody  = "body"
+	prmRealm = "realm"
+)
+
+// MakeCommunicationsHandler make an HTTP handler for a Communications endpoint.
+func MakeCommunicationsHandler(e endpoint.Endpoint, logger log.Logger) *http_transport.Server {
+	return http_transport.NewServer(e,
+		decodeCommunicationsRequest,
+		commonhttp.EncodeReply,
+		http_transport.ServerErrorEncoder(commonhttp.ErrorHandler(logger)),
+	)
+}
+
+// decodeCommunicationsRequest gets the HTTP parameters and body content
+func decodeCommunicationsRequest(ctx context.Context, req *http.Request) (interface{}, error) {
+	var pathParams = map[string]string{
+		prmRealm: constants.RegExpRealmName,
+	}
+
+	var queryParams = map[string]string{}
+
+	return commonhttp.DecodeRequest(ctx, req, pathParams, queryParams)
+}
