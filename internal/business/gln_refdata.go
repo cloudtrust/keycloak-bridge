6e25fd1bb73655e8b42f2bfabed6fd8fa69e8a58
Francis PEROT <francis.perot@elca.ch>
2021-04-19 15:29:02 +0200

    [CLOUDTRUST-3114] Uses SOAP API exposed by refdata.ch for GLN

--- internal/business/gln_refdata.go
+++ internal/business/gln_refdata.go
@@ -2,6 +2,7 @@ package business
 
 import (
 	"context"
+	"encoding/xml"
 	"fmt"
 	"regexp"
 	"strings"
@@ -16,143 +17,145 @@ import (
 	"gopkg.in/h2non/gentleman.v2/plugin"
 	"gopkg.in/h2non/gentleman.v2/plugins/body"
 	"gopkg.in/h2non/gentleman.v2/plugins/headers"
-	"gopkg.in/h2non/gentleman.v2/plugins/query"
-	gurl "gopkg.in/h2non/gentleman.v2/plugins/url"
 )
 
 var (
-	headerParser = regexp.MustCompile(`th scope="col".[^>]+>([^<]+)<`)
-	resultParser = regexp.MustCompile(`<td[^>]*>([^<]+)`)
-	npaParser    = regexp.MustCompile(`^(\d+)\s+(.+)$`)
+	soapRequest = strings.Trim(`
+	<?xml version="1.0" encoding="utf-8"?>
+	<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+	  <soap:Body>
+		<DownloadPartnerInput xmlns="http://refdatabase.refdata.ch/">
+		  <TYPE xmlns="http://refdatabase.refdata.ch/Partner_in">GLN</TYPE>
+		  <PTYPE xmlns="http://refdatabase.refdata.ch/Partner_in">ALL</PTYPE>
+		  <TERM xmlns="http://refdatabase.refdata.ch/Partner_in">##GLN##</TERM>
+		</DownloadPartnerInput>
+	  </soap:Body>
+	</soap:Envelope>
+	`, " \r\n\t")
 )
 
-const (
-	resultStart       = `<tbody class="dgRow status`
-	resultEnd         = `</tbody`
-	allResultsEnd     = `</tabl`
-	MsgErrCannotParse = "cannotParse"
-)
+type SoapPartnerResponse struct {
+	XMLName xml.Name    `xml:"http://schemas.xmlsoap.org/soap/envelope/ Envelope"`
+	XSI     *string     `xml:"xmlns:xsi,attr"`
+	XSD     *string     `xml:"xmlns:xsd,attr"`
+	Header  interface{} `xml:"http://schemas.xmlsoap.org/soap/envelope/ Header"`
+	Body    struct {
+		Partner struct {
+			Creation  *string `xml:"CREATION_DATETIME,attr"`
+			Namespace *string `xml:"xmlns,attr"`
+			Result    struct {
+				Namespace *string `xml:"xmlns,attr"`
+				OkError   *string `xml:"OK_ERROR"`
+				NbrRecord *int    `xml:"NBR_RECORD"`
+			} `xml:"RESULT"`
+			Items []SoapItem `xml:"ITEM"`
+		} `xml:"PARTNER"`
+	} `xml:"Body"`
+}
+
+type SoapItem struct {
+	Date        *string `xml:"DT,attr"`
+	Namespace   *string `xml:"xmlns,attr"`
+	PartnerType *string `xml:"PTYPE,omitempty"`
+	GLN         *string `xml:"GLN,omitempty"`
+	Status      *string `xml:"STATUS,omitempty"`
+	StartDate   *string `xml:"STDATE,omitempty"`
+	Language    *string `xml:"LANG,omitempty"`
+	LastName    *string `xml:"DESCR1,omitempty"`
+	FirstName   *string `xml:"DESCR2,omitempty"`
+	Role        struct {
+		Type    *string `xml:"TYPE,omitempty"`
+		ZipCode *string `xml:"ZIP,omitempty"`
+		City    *string `xml:"CITY,omitempty"`
+		Canton  *string `xml:"CTN,omitempty"`
+		Country *string `xml:"CNTRY,omitempty"`
+	} `xml:"ROLE,omitempty"`
+}
 
 type glnRefData struct {
-	client         *httpclient.Client
-	origin         string
-	searchPagePath string
-	formPath       string
-	referer        string
-	logger         keycloakb.Logger
+	client     *httpclient.Client
+	soapAction string
+	logger     keycloakb.Logger
 }
 
 // NewRefDataLookup creates a GLN lookup tool using refdata.ch
 func NewRefDataLookup(baseURL string, httpTimeout time.Duration, logger keycloakb.Logger) (GlnLookupProvider, error) {
-	// Search page: https://www.refdata.ch/fr/partenaires/requete/base-de-donnees-des-partenaires-gln
-	// Form: https://refdatabase.refdata.ch/Viewer/SearchPartnerByGln?Lang=fr
-	// Origin: https://refdatabase.refdata.ch
-	// Referer: https://refdatabase.refdata.ch/Viewer/Partner/?Lang=FR
 	var client, err = httpclient.New(baseURL, httpTimeout)
 	if err != nil {
 		return nil, err
 	}
 
+	var soapAction = "http://refdatabase.refdata.ch/Download"
+	re := regexp.MustCompile(`/`)
+	idx := re.FindAllIndex([]byte(baseURL), 3)
+	if len(idx) > 2 {
+		// First 2 slashes are the ones between the protocol and the server name. We split starting of the third one
+		soapAction = strings.ReplaceAll(baseURL[0:idx[2][0]], "https", "http") + "/Download"
+	}
+
 	return &glnRefData{
-		client:         client,
-		origin:         baseURL,
-		searchPagePath: "/fr/partenaires/requete/base-de-donnees-des-partenaires-gln",
-		formPath:       "/Viewer/SearchPartnerByGln",
-		referer:        baseURL + "/Viewer/Partner/?Lang=FR",
-		logger:         logger,
+		client:     client,
+		soapAction: soapAction,
+		logger:     logger,
 	}, nil
 }
 
 func (l *glnRefData) Lookup(gln string) GlnSearchResult {
-	if html, err := l.request(gln); err != nil {
+	if bytes, err := l.request(gln); err != nil {
 		return GlnSearchResult{Error: err}
 	} else {
-		return l.htmlToDetails(gln, html)
+		return l.xmlToDetails(gln, bytes)
 	}
 }
 
-func (l *glnRefData) request(gln string) (string, error) {
-	var bodyValue = fmt.Sprintf("SearchGln=%s&Sort=&NewSort=&IsAscending=False&Reset=False", gln)
+func (l *glnRefData) request(gln string) ([]byte, error) {
+	var bodyValue = strings.ReplaceAll(soapRequest, "##GLN##", gln)
 	var plugins []plugin.Plugin
 	plugins = append(plugins,
-		gurl.Path(l.formPath),
-		query.Add("Lang", "fr"),
+		headers.Set("Content-Type", "text/xml; charset=utf-8"),
 		headers.Set("Content-Length", fmt.Sprintf("%d", utf8.RuneCountInString(bodyValue))),
-		headers.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8"),
-		headers.Set("Origin", l.origin),
-		headers.Set("Referer", l.referer),
+		headers.Set("SOAPAction", l.soapAction),
 		body.String(bodyValue),
 	)
 
-	var response string
+	var response []byte
 	if _, err := l.client.Post(&response, plugins...); err != nil {
-		l.logger.Warn(context.Background(), "msg", "Can't get response from psyReg", "err", err.Error(), "gln", gln)
-		return "", errors.Wrap(err, keycloak.MsgErrCannotObtain+"."+keycloak.Response)
+		l.logger.Warn(context.Background(), "msg", "Can't get response from refData", "err", err.Error(), "gln", gln)
+		return nil, errors.Wrap(err, keycloak.MsgErrCannotObtain+"."+keycloak.Response)
 	} else {
 		return response, nil
 	}
 }
 
-func (l *glnRefData) htmlToDetails(gln string, html string) GlnSearchResult {
-	// Get headers
-	var matchedHeaders = headerParser.FindAllStringSubmatch(html, -1)
-	var resHeaders []string
-	for _, m := range matchedHeaders {
-		resHeaders = append(resHeaders, m[1])
+func (l *glnRefData) xmlToDetails(gln string, xmlContent []byte) GlnSearchResult {
+	var response SoapPartnerResponse
+	if err := xml.Unmarshal(xmlContent, &response); err != nil {
+		return GlnSearchResult{Error: errorhandler.CreateInternalServerError("gln.cantParseXML")}
 	}
-
-	if len(resHeaders) == 0 {
+	if response.Body.Partner.Result.OkError == nil || *response.Body.Partner.Result.OkError != "OK" {
+		l.logger.Warn(context.Background(), "msg", "Failed to request GLN through refdata", "gln", gln)
+		return GlnSearchResult{Error: errorhandler.CreateInternalServerError("gln.requestError")}
+	}
+	if response.Body.Partner.Result.NbrRecord == nil || *response.Body.Partner.Result.NbrRecord == 0 {
 		return GlnSearchResult{Error: ErrGLNNotFound}
 	}
-
-	var start = strings.Index(html, resultStart)
-	var end = strings.LastIndex(html, allResultsEnd)
-	if start < 0 || end < start {
-		return GlnSearchResult{Error: errorhandler.CreateInternalServerError("gln.refData.invalidResponse")}
-	}
-
 	var result []GlnPerson
-	for _, personHtml := range strings.Split(html[start+1:end], resultStart) {
-		var matchedDetails = resultParser.FindAllStringSubmatch(personHtml, -1)
-		var maxIndex = len(matchedDetails)
-		if maxIndex > len(resHeaders) {
-			maxIndex = len(resHeaders)
-		}
-		var details = make(map[string]string)
-		for i := 0; i < maxIndex; i++ {
-			details[resHeaders[i]] = matchedDetails[i][1]
-		}
-
-		var strActive = l.getValue(details, "Statut")
-		var active = strActive != nil && *strActive == "A"
-		var person = GlnPerson{
+	for _, item := range response.Body.Partner.Items {
+		var active = item.Status != nil && *item.Status == "A"
+		result = append(result, GlnPerson{
 			Active:     &active,
-			Number:     l.getValue(details, "GLN"),
-			FirstName:  l.getValue(details, "Pr&#233;nom"),
-			LastName:   l.getValue(details, "Nom"),
-			Canton:     l.getValue(details, "Ctn"),
-			ZipCode:    nil,
-			City:       nil,
-			Country:    l.getValue(details, "Pays"),
-			Profession: l.getValue(details, "Profession"),
-		}
-		var npa = l.getValue(details, "NPA")
-		if npa != nil {
-			var matched = npaParser.FindAllStringSubmatch(*npa, -1)
-			if len(matched) == 1 && len(matched[0]) == 3 {
-				person.ZipCode = &matched[0][1]
-				person.City = &matched[0][2]
-			}
-		}
-		result = append(result, person)
+			Number:     item.GLN,
+			FirstName:  item.FirstName,
+			LastName:   item.LastName,
+			ZipCode:    item.Role.ZipCode,
+			City:       item.Role.City,
+			Canton:     item.Role.Canton,
+			Country:    item.Role.Country,
+			Profession: item.Role.Type,
+		})
 	}
-
-	return GlnSearchResult{Persons: result, Error: nil}
-}
-
-func (l *glnRefData) getValue(details map[string]string, field string) *string {
-	if value, ok := details[field]; ok {
-		return &value
+	return GlnSearchResult{
+		Persons: result,
+		Error:   nil,
 	}
-	return nil
 }
