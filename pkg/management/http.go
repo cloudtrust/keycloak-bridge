1b15ed78769fd7b3d96d611000ccc125c2a4a3bb
Francis PEROT <francis.perot@elca.ch>
2021-05-26 14:28:21 +0200

    [SCPR-109] Allow more characters for LastName search

--- pkg/management/http.go
+++ pkg/management/http.go
@@ -71,7 +71,7 @@ func decodeManagementRequest(ctx context.Context, req *http.Request) (interface{
 	var queryParams = map[string]string{
 		prmQryEmail:       constants.RegExpEmail,
 		prmQryFirstName:   constants.RegExpFirstName,
-		prmQryLastName:    constants.RegExpLastName,
+		prmQryLastName:    constants.RegExpLastNameSearch,
 		prmQryUserName:    constants.RegExpUsername,
 		prmQrySearch:      constants.RegExpSearch,
 		prmQryClientID:    constants.RegExpClientID,
