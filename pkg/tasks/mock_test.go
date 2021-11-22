7f0d18040ec4b92133917e7b983ba5f48df23ae7
Bastien Beuchat <bastien.beuchat@gmail.com>
2021-11-22 14:45:25 +0100

    [CLOUDTRUST-3881] Correct go generate cmds (#363)
    * [CLOUDTRUST-3881] Correct go generate cmds

--- pkg/tasks/mock_test.go
+++ pkg/tasks/mock_test.go
@@ -1,6 +1,6 @@
 package tasks
 
-//go:generate mockgen -destination=./mock/component.go -package=mock -mock_names=Component=Component,KeycloakClient=KeycloakClient,UsersDetailsDBModule=UsersDetailsDBModule github.com/cloudtrust/keycloak-bridge/pkg/tasks Component,KeycloakClient,UsersDetailsDBModule
-//go:generate mockgen -destination=./mock/authentication_db_reader.go -package=mock -mock_names=AuthorizationDBReader=AuthorizationDBReader github.com/cloudtrust/common-service/security AuthorizationDBReader
-//go:generate mockgen -destination=./mock/kc-auth.go -package=mock -mock_names=KeycloakClient=KcClientAuth github.com/cloudtrust/common-service/security KeycloakClient
-//go:generate mockgen -destination=./mock/dbase.go -package=mock -mock_names=EventsDBModule=EventsDBModule github.com/cloudtrust/common-service/database EventsDBModule
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/component.go -package=mock -mock_names=Component=Component,KeycloakClient=KeycloakClient,UsersDetailsDBModule=UsersDetailsDBModule github.com/cloudtrust/keycloak-bridge/pkg/tasks Component,KeycloakClient,UsersDetailsDBModule
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/authentication_db_reader.go -package=mock -mock_names=AuthorizationDBReader=AuthorizationDBReader github.com/cloudtrust/common-service/security AuthorizationDBReader
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/kc-auth.go -package=mock -mock_names=KeycloakClient=KcClientAuth github.com/cloudtrust/common-service/security KeycloakClient
+//go:generate mockgen --build_flags=--mod=mod -destination=./mock/dbase.go -package=mock -mock_names=EventsDBModule=EventsDBModule github.com/cloudtrust/common-service/database EventsDBModule
