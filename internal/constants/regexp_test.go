05e955ae42e8b33ace87fc3a92f4c5c0e62968c2
sispeo <42068883+fperot74@users.noreply.github.com>
2022-01-13 11:12:58 +0100

    [CLOUDTRUST-4009] Allow comma in first name values. Make it be configurable

--- internal/constants/regexp_test.go
+++ internal/constants/regexp_test.go
@@ -12,7 +12,29 @@ func matches(regex string, value string) bool {
 	return res
 }
 
+func TestInit(t *testing.T) {
+	var tests = map[string]func() string{
+		"first-name":     func() string { return RegExpFirstName },
+		"last-name":      func() string { return RegExpLastName },
+		"country-code":   func() string { return RegExpCountryCode },
+		"birth-location": func() string { return RegExpBirthLocation },
+		"id-doc-number":  func() string { return RegExpIDDocumentNumber },
+	}
+	var newRegex = "^.+$"
+	for k, v := range tests {
+		var original = v()
+		InitializeRegexOverride(map[string]string{k: newRegex})
+		assert.NotEqual(t, original, v())
+
+		InitializeRegexOverride(map[string]string{k: original})
+		assert.Equal(t, original, v())
+	}
+}
+
 func TestRegex(t *testing.T) {
+	assert.True(t, matches(RegExpFirstName, "CLÉMENT, ROMAIN, HUGO"))
+	assert.False(t, matches(RegExpFirstName, ",LÉMENT, ROMAIN, HUGO"))
+
 	assert.True(t, matches(RegExpBirthLocation, "àáâäçèéêëìíîïñòóôöùúûüßÀÁÂÄÇÈÉÊËÌÍÎÏÑÒÓÔÖÙÚÛÜÆæŒœ"))
 	assert.True(t, matches(RegExpBirthLocation, "Wrocław"))
 	assert.True(t, matches(RegExpBirthLocation, "Владивосток"))
