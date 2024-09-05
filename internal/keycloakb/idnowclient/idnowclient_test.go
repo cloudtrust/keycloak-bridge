cad54b705422332206fdb39a328a0ef86d08c877
Bastien BEUCHAT <bastien.beuchat@elca.ch>
2024-09-05 18:24:43 +0200

    [CLOUDTRUST-5482] Call idnow service to get identifications statistics

---
+++ internal/keycloakb/idnowclient/idnowclient_test.go
@@ -0,0 +1,40 @@
+package idnowclient
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	cs "github.com/cloudtrust/common-service/v2"
+	"github.com/cloudtrust/keycloak-bridge/internal/keycloakb/mock"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetIdentificationsByType(t *testing.T) {
+	var mockCtrl = gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockHTTPClient := mock.NewHTTPClient(mockCtrl)
+	idnowClient := MakeIdnowServiceClient(mockHTTPClient)
+	var ctx = context.Background()
+	var expectedError = errors.New("Test error")
+	var correlationID = "TestCorrelationID"
+	var realm = "testRealm"
+
+	ctx = context.WithValue(ctx, cs.CtContextCorrelationID, correlationID)
+
+	t.Run("SUCCESS", func(t *testing.T) {
+		mockHTTPClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any())
+
+		_, err := idnowClient.GetIdentificationsByType(ctx, realm)
+		assert.Nil(t, err)
+	})
+
+	t.Run("FAILURE", func(t *testing.T) {
+		mockHTTPClient.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(expectedError)
+
+		_, err := idnowClient.GetIdentificationsByType(ctx, realm)
+		assert.NotNil(t, err)
+	})
+}
--- pkg/statistics/component_test.go
+++ pkg/statistics/component_test.go
@@ -10,30 +10,29 @@ import (
 	"github.com/cloudtrust/common-service/v2/security"
 	api "github.com/cloudtrust/keycloak-bridge/api/statistics"
 	"github.com/cloudtrust/keycloak-bridge/internal/keycloakb/accreditationsclient"
+	"github.com/cloudtrust/keycloak-bridge/internal/keycloakb/idnowclient"
 	"github.com/cloudtrust/keycloak-bridge/pkg/statistics/mock"
 	kc "github.com/cloudtrust/keycloak-client/v2"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
 
-func ptr(value string) *string {
-	return &value
-}
-
 type componentMocks struct {
 	keycloakClient *mock.KcClient
 	accredsService *mock.AccreditationsServiceClient
+	idnowService   *mock.IdnowServiceClient
 }
 
 func newComponentMocks(mockCtrl *gomock.Controller) *componentMocks {
 	return &componentMocks{
 		keycloakClient: mock.NewKcClient(mockCtrl),
 		accredsService: mock.NewAccreditationsServiceClient(mockCtrl),
+		idnowService:   mock.NewIdnowServiceClient(mockCtrl),
 	}
 }
 
 func (cm *componentMocks) newComponent() *component {
-	return NewComponent(cm.keycloakClient, cm.accredsService, log.NewNopLogger()).(*component)
+	return NewComponent(cm.keycloakClient, cm.accredsService, cm.idnowService, log.NewNopLogger()).(*component)
 }
 
 func TestGetStatisticsIdentifications(t *testing.T) {
@@ -43,13 +42,21 @@ func TestGetStatisticsIdentifications(t *testing.T) {
 	var mocks = newComponentMocks(mockCtrl)
 	var component = mocks.newComponent()
 
-	var errDbModule = errors.New("Dummy error in db module")
+	var testError = errors.New("test error")
 	var realm = "the_realm_name"
 	var expected = api.IdentificationStatisticsRepresentation{}
 	var ctx = context.Background()
 
-	t.Run("db error", func(t *testing.T) {
-		mocks.accredsService.EXPECT().GetIdentityChecksByNature(ctx, realm).Return([]accreditationsclient.NatureCheckCount{}, errDbModule)
+	t.Run("accreditation service error", func(t *testing.T) {
+		mocks.accredsService.EXPECT().GetIdentityChecksByNature(ctx, realm).Return([]accreditationsclient.NatureCheckCount{}, testError)
+
+		_, err := component.GetStatisticsIdentifications(ctx, realm)
+		assert.NotNil(t, err)
+	})
+
+	t.Run("idnow service error", func(t *testing.T) {
+		mocks.accredsService.EXPECT().GetIdentityChecksByNature(ctx, realm).Return([]accreditationsclient.NatureCheckCount{}, nil)
+		mocks.idnowService.EXPECT().GetIdentificationsByType(ctx, realm).Return(idnowclient.IdentificationStatistics{}, testError)
 
 		_, err := component.GetStatisticsIdentifications(ctx, realm)
 		assert.NotNil(t, err)
@@ -64,8 +71,13 @@ func TestGetStatisticsIdentifications(t *testing.T) {
 		mocks.accredsService.EXPECT().GetIdentityChecksByNature(ctx, realm).Return([]accreditationsclient.NatureCheckCount{
 			{Nature: ptr("PHYSICAL_CHECK"), Count: &expected.PhysicalIdentifications},
 			{Nature: ptr("BASIC_CHECK"), Count: &expected.BasicIdentifications},
-			{Nature: ptr("IDNOW_CHECK"), Count: &expected.VideoIdentifications},
-			{Nature: ptr("AUTO_IDENT_IDNOW_CHECK"), Count: &expected.AutoIdentifications},
+			{Nature: ptr("IDNOW_CHECK"), Count: intPtr(48)},
+			{Nature: ptr("AUTO_IDENT_IDNOW_CHECK"), Count: intPtr(19)},
+		}, nil)
+
+		mocks.idnowService.EXPECT().GetIdentificationsByType(ctx, realm).Return(idnowclient.IdentificationStatistics{
+			VideoIdentifications: expected.VideoIdentifications,
+			AutoIdentifications:  expected.AutoIdentifications,
 		}, nil)
 
 		res, err := component.GetStatisticsIdentifications(ctx, realm)
