7961832764ae0f419932a286382428a4ba3eea01
Jonas Troeltsch <jonas.troeltsch@elca.ch>
2025-10-27 13:31:00 +0100

    [CLOUDTRUST-7151] Fix context key management issue

--- pkg/register/component.go
+++ pkg/register/component.go
@@ -182,8 +182,8 @@ func (c *component) RegisterUser(ctx context.Context, targetRealmName string, cu
 	var redirect = false
 	var ctxOverride keycloakb.ContextKeyParameters
 	if contextKey != nil {
-		if ctxOverride, err = c.contextKeyMgr.GetOverride(ctx, *contextKey, targetRealmName); err != nil {
-			c.logger.Info(ctx, "msg", "Invalid context key", "context-key", *contextKey, "err", err.Error(), "realm", targetRealmName)
+		if ctxOverride, err = c.contextKeyMgr.GetOverride(ctx, *contextKey, customerRealmName); err != nil {
+			c.logger.Info(ctx, "msg", "Invalid context key", "context-key", *contextKey, "err", err.Error(), "realm", customerRealmName)
 			return "", errorhandler.CreateBadRequestError(errorhandler.MsgErrInvalidParam + ".context-key")
 		}
 		if ctxOverride.OnboardingClientID != nil {
